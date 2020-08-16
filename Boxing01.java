986d84d586378c7bbeb9f2a8f518af9b8689a8e8
harakoji05 <harakoji.0514@gmail.com>
2020-08-16 11:25:47 +0900

    first commit

---
+++ Boxing01.java
@@ -0,0 +1,12 @@
+
+public class Boxing01 {
+	public static void main(String[] args) {
+		Integer integer1 = 5;
+		Integer integer2 = Integer.value0f(5);
+		int int1 = integer1;
+		int int2 = integer2.intValue();
+
+		System.out.println(int1);
+		System.out.println(int2);
+	}
+}
---
+++ Chapter05.java
@@ -0,0 +1,24 @@
+
+
+public class Chapter05 {
+	public static void main(String[] args) {
+
+		long int1 = 150;
+		double int2 = 200.005;
+		boolean  boolean1 = true;
+
+		System.out.println(int1);
+		System.out.println(int2);
+		System.out.println(boolean1);
+
+		Integer int4 = 100;
+        System.out.println(int4);
+        Float float1 = 1.4142f;
+        System.out.println(float1);
+        Character character1 = 'あ';
+        System.out.println(character1);
+
+        int[] array = { 0, 10, 20, 30, 40 };
+        System.out.println(array[3]);
+    }
+}
